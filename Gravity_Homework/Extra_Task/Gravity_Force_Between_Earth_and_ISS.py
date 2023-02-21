911da40ce250ca235835ed3cd9d052313bc73203
m1k3im <tonya1337maks@yandex.ru>
2023-02-22 02:21:57 +0300

    Update Gravity_Force_Between_Earth_and_ISS.py

--- Gravity_Homework/Extra_Task/Gravity_Force_Between_Earth_and_ISS.py
+++ Gravity_Homework/Extra_Task/Gravity_Force_Between_Earth_and_ISS.py
@@ -2,23 +2,21 @@ import urllib.request
 import json
 
 
-req = urllib.request.Request("https://api.wheretheiss.at/v1/satellites/25544") ## api input
+req = urllib.request.Request("https://api.wheretheiss.at/v1/satellites/25544")  # api input
 response = urllib.request.urlopen(req)
-obj = json.loads(response.read()) ## write api parameters
+obj = json.loads(response.read())  # write api parameters
 
 
-altitude = (obj['altitude'])*10**3+(6371*10**3) ## counting the distance between Earth and ISS
+altitude = (obj['altitude'])*10**3+(6371*10**3)  # counting the distance between Earth and ISS
 earth_weight = 5.972*10**24
-g = 6.67408*10**-11 ## Gravitational constant
+g = 6.67408*10**-11  # Gravitational constant
 
-## Function to calculate the gravitational force
+# Function to calculate gravity force
+
+
 def math_gravitation(other_object_weight, distance_between):
     return (g * earth_weight * other_object_weight) / (distance_between**2)
 
 
-force = math_gravitation(420*10**3,altitude) ## Count the force
-print("The force between The ISS and The Earth is ~", round(force), "Newtons at the moment") ## Final Result
-
-
-
-
+force = math_gravitation(420*10**3, altitude)  # Count the force
+print("The force between The ISS and The Earth is ~", round(force), "Newtons at the moment")  # Final Result
